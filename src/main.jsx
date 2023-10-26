afe40be2b81b6d3add66aeb35c6e05fa64775850
Christian <christiancamiloriveralamadrid@gmail.com>
2023-10-26 14:12:56 -0500

    Last changes

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { store } from "./store/store.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/JournalApp" >
         <JournalApp />
       </BrowserRouter>
     </Provider>
